1e9f0b7f89c7ed024e37d5f563670c43f0c2e199
Ravi Singh <training.appsimpact@gmail.com>
2023-05-05 10:44:57 +0530

    Fixed nagtive value issue

--- app/models/leave_application.rb
+++ app/models/leave_application.rb
@@ -76,7 +76,7 @@ class LeaveApplication < ApplicationRecord
     return if leave_type_id.blank?
     return if start_date.blank? || end_date.blank?
 
-    if leave_type.short_name == 'CL' && employee.current_leave_summary.cl_available < total_leaves_applied
+    if leave_type.short_name == 'CL' && employee.current_leave_summary.cl_available < total_leaves_applied || leave_type.short_name == 'CL' && is_half_day && employee.current_leave_summary.cl_available == 0.0
       errors.add(:you,  "do not have enough casual leave balance. (Available #{employee.current_leave_summary.cl_available} but applied #{total_leaves_applied})")
     end
   end
@@ -85,7 +85,7 @@ class LeaveApplication < ApplicationRecord
     return if leave_type_id.blank?
     return if start_date.blank? || end_date.blank?
 
-    if leave_type.short_name == 'SL' && employee.current_leave_summary.sl_available < total_leaves_applied
+    if leave_type.short_name == 'SL' && employee.current_leave_summary.sl_available < total_leaves_applied || leave_type.short_name == 'SL' && is_half_day && employee.current_leave_summary.sl_available == 0.0
       errors.add(:you,  "do not have enough sick leave balance. (Available #{employee.current_leave_summary.sl_available} but applied #{total_leaves_applied})")
     end
   end
@@ -94,7 +94,7 @@ class LeaveApplication < ApplicationRecord
     return if leave_type_id.blank?
     return if start_date.blank? || end_date.blank?
 
-    if leave_type.short_name == 'EL' && employee.current_leave_summary.el_available < total_leaves_applied
+    if leave_type.short_name == 'EL' && employee.current_leave_summary.el_available < total_leaves_applied || leave_type.short_name == 'EL' && is_half_day && employee.current_leave_summary.el_available == 0.0
       errors.add(:you,  "do not have enough earned leave balance. (Available #{employee.current_leave_summary.el_available} but applied #{total_leaves_applied})")
     end
   end
@@ -103,7 +103,7 @@ class LeaveApplication < ApplicationRecord
     return if leave_type_id.blank?
     return if start_date.blank? || end_date.blank?
 
-    if leave_type.short_name == 'OL' && employee.current_leave_summary.ol_available < total_leaves_applied
+    if leave_type.short_name == 'OL' && employee.current_leave_summary.ol_available < total_leaves_applied || leave_type.short_name == 'OL' && is_half_day && employee.current_leave_summary.ol_available == 0.0
       errors.add(:you,  "do not have enough optional leave balance. (Available #{employee.current_leave_summary.ol_available} but applied #{total_leaves_applied})")
     end
   end
@@ -112,7 +112,7 @@ class LeaveApplication < ApplicationRecord
     return if leave_type_id.blank?
     return if start_date.blank? || end_date.blank?
 
-    if leave_type.short_name == 'PL' && employee.current_leave_summary.pl_available < total_leaves_applied
+    if leave_type.short_name == 'PL' && employee.current_leave_summary.pl_available < total_leaves_applied || leave_type.short_name == 'PL' && is_half_day && employee.current_leave_summary.pl_available == 0.0
       errors.add(:you,  "do not have enough paternity leave balance. (Available #{employee.current_leave_summary.pl_available} but applied #{total_leaves_applied})")
     end
   end
@@ -121,7 +121,7 @@ class LeaveApplication < ApplicationRecord
     return if leave_type_id.blank?
     return if start_date.blank? || end_date.blank?
 
-    if leave_type.short_name == 'ML' && employee.current_leave_summary.ml_available < total_leaves_applied
+    if leave_type.short_name == 'ML' && employee.current_leave_summary.ml_available < total_leaves_applied || leave_type.short_name == 'ML' && is_half_day && employee.current_leave_summary.ml_available == 0.0
       errors.add(:you,  "do not have enough maternity leave balance. (Available #{employee.current_leave_summary.ml_available} but applied #{total_leaves_applied})")
     end
   end
@@ -130,27 +130,27 @@ class LeaveApplication < ApplicationRecord
     leave_summary = employee.current_leave_summary
     case leave_type.short_name
     when 'CL'
-      cl_taken = leave_summary.cl_taken + total_leaves_applied
+      cl_taken = is_half_day ? leave_summary.cl_taken + 0.5 : leave_summary.cl_taken + total_leaves_applied
       cl_available = is_half_day ? leave_summary.cl_available - 0.5 : leave_summary.cl_available - total_leaves_applied 
       leave_summary.update(cl_taken: cl_taken, cl_available: cl_available) 
     when 'EL'
-      el_taken = leave_summary.el_taken + total_leaves_applied
+      el_taken = is_half_day ? leave_summary.el_taken + 0.5 : leave_summary.el_taken + total_leaves_applied
       el_available = is_half_day ? leave_summary.el_available - 0.5 : leave_summary.el_available - total_leaves_applied
       leave_summary.update(el_taken: el_taken, el_available: el_available)
     when 'SL'
-      sl_taken = leave_summary.sl_taken + total_leaves_applied
+      sl_taken = is_half_day ? leave_summary.sl_taken + 0.5 : leave_summary.sl_taken + total_leaves_applied
       sl_available = is_half_day ? leave_summary.sl_available - 0.5 : leave_summary.sl_available - total_leaves_applied 
       leave_summary.update(sl_taken: sl_taken, sl_available: sl_available)
     when 'OL'
-      ol_taken = leave_summary.ol_taken + total_leaves_applied
+      ol_taken = is_half_day ? leave_summary.ol_taken + 0.5 : leave_summary.ol_taken + total_leaves_applied
       ol_available = is_half_day ? leave_summary.ol_available - 0.5 : leave_summary.ol_available - total_leaves_applied
       leave_summary.update(ol_taken: ol_taken, ol_available: ol_available)
     when 'PL'
-      pl_taken = leave_summary.pl_taken + total_leaves_applied
+      pl_taken = is_half_day ? leave_summary.pl_taken + 0.5 : leave_summary.pl_taken + total_leaves_applied
       pl_available = is_half_day ? leave_summary.pl_available - 0.5 : leave_summary.pl_available - total_leaves_applied
       leave_summary.update(pl_taken: pl_taken, pl_available: pl_available)
     when 'ML'
-      ml_taken = leave_summary.ml_taken + total_leaves_applied
+      ml_taken = is_half_day ? leave_summary.ml_taken + 0.5 : leave_summary.ml_taken + total_leaves_applied
       ml_available = is_half_day ? leave_summary.ml_available - 0.5 : leave_summary.ml_available - total_leaves_applied
       leave_summary.update(ml_taken: ml_taken, ml_available: ml_available)  
     else
