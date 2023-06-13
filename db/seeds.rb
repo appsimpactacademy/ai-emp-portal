ad8f1d7a1ff8cbfc85bb8063203269b37bd00001
Ravi Kashyap <ravikashyap@Ravis-MacBook-Air.local>
2023-06-13 10:35:27 +0530

    quick fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-10.times do |i|
+1000.times do |i|
 	ActiveRecord::Base.transaction do 
 		employee = Employee.create!(
 			first_name: Faker::Name.first_name,
